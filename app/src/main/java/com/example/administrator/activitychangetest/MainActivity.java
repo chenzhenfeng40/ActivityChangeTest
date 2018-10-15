aa7f315e6bdc6f3d65b30aada9a19177a78c4baa
chenzhenfeng40 <771050668@qq.com>
2018-10-15 17:20:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/activitychangetest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.administrator.activitychangetest;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+private Button button1;
+ //   button1 = (Button)findViewByld(R.buttonA);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button)findViewById(R.id.buttonA);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
